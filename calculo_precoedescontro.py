443c372dfb3fdb8b598269dce37b4c2854664ecd
DevPython360 <luiseduardo071@gmail.com>
2025-03-10 21:46:42 -0300

    Add files via upload

---
+++ calculo_precoedescontro.py
@@ -0,0 +1,45 @@
+## Calculo de deconto em uma loja:
+
+preco = 150.0
+desconto = 20
+print ('subtração:' , preco - desconto)
+print ('Divisão:' , 130 / 150.0)
+print ('O Desconto é:' , 0.867 * 100)
+
+## Cáuculo de IMC (índice de Massa corporea):
+
+peso = 70
+Altura = 1.75
+indicie_massa= ("Resultado do IMC:" , 70 / 1.75)
+print (indicie_massa)
+
+## Verificação de elegibilidade com valores booleanos:
+
+idade = 22
+tem_experiencia = True
+elegivel_para_concurso = idade >= 18 and tem_experiencia
+print ('É elegível:', elegivel_para_concurso)
+
+## Classificação de de Níveis de acesso com estruturas condicionais:
+
+nivel_acesso = int(input('Informe Seu Nivel'))
+
+if nivel_acesso == 3: 
+   print('Acesso Total')
+elif nivel_acesso == 2:
+    print('Acesso Parcial')
+else: 
+    print ('Acesso Restrito')
+
+## Conversão de graus celsius em fahrenheit:
+
+celsius = 70
+print ('Conversão para fahrenheit' , 70 * 9/5 + 32)
+
+
+
+
+
+
+
+
