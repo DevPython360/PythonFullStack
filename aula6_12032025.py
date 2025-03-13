9a8a190b7d891ca627c0b7daaf2e67d8bd40a647
DevPython360 <luiseduardo071@gmail.com>
2025-03-12 21:41:03 -0300

    Add files via upload

---
+++ aula6_12032025.py
@@ -0,0 +1,16 @@
+alunopython ="Andrei"
+for i in alunopython:
+    print(i)
+
+for i in range(1,6):
+    print(i)
+    
+
+alunopython="Andrei"
+lista_alunospython=["Alyfer" , "Arthur" , "Andressa", "Andrei"]
+for letra in lista_alunospython:
+    if letra == alunopython:
+        print("Aluno matriculado")
+    else:
+        print("Aluno não matriculado")    
+
